59d97d7249a91662faeb7c66a118ea6d04911c66
askdfjlas <kaihengd@gmail.com>
2022-03-26 16:49:01 -0400

    Splash screen

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter, Route, Routes } from 'react-router-native';
@@ -5,16 +6,16 @@ import Home from './components/Home/Home';
 import SignUp from './components/SignUp/SignUp';
 
 export default function App() {
-  return (
-    <NativeRouter>
-        <View style={styles.container}>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/signUp" element={<SignUp />} />
-            </Routes>
-        </View>
-    </NativeRouter>
-  );
+    return (
+        <NativeRouter>
+            <View style={styles.container}>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/signUp" element={<SignUp />} />
+                </Routes>
+            </View>
+        </NativeRouter>
+    );
 }
 
 const styles = StyleSheet.create({
